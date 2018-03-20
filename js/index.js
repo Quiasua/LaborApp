e65f7206015acefbe7b2e411afb5e04ef8ad95ff
acaucali <31599293+acaucali@users.noreply.github.com>
2018-03-20 10:06:57 -0500

    cambios  acaucali 20/03/18

--- js/index.js
+++ js/index.js
@@ -47,3 +47,15 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+// SIDEBAR
+$(document).ready(function(){
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  // START OPEN
+  $('.button-collapse').sideNav('show');
+});
--- materialize-sidenav/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/j_holtslander/pen/jmgZPQ.
-
- Part of Jay's Materialize Collection at: https://codepen.io/collection/nbBqgY
