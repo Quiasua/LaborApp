33d8f25486d645a0d7d106efe5d1ad6aeb8a5f8e
jmontanabonilla <jmontanabonilla@gmail.com>
2018-04-28 16:14:34 -0500

    commit

--- js/funciones-materialize.js
+++ js/funciones-materialize.js
@@ -1,3 +1,12 @@
-function showMessage(value) {
-    document.getElementById("message").innerHTML  =  value;
- }
+$(document).ready(function(){ //Funcion al momento de recargar la página
+    $("#checkbox2").click( function(){
+        if( $(this).is(':checked') ){
+            $("#otroSalario,#tituloOtroSalario").css("display","block"); 
+           // $("input,#select-salario").prop('disabled', true);
+           $('select#salario').attr('disabled','disabled');   
+        }
+     });
+     
+});
+
+
