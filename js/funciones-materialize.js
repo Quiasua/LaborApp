b99e52e4b1a775f76fac19b3f8441af85badc457
jmontanabonilla <jmontanabonilla@gmail.com>
2018-04-29 15:28:28 -0500

    commit

--- js/funciones-materialize.js
+++ js/funciones-materialize.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){ //Funcion al momento de recargar la página
-    $("#checkbox2").click( function(){
-        if( $(this).is(':checked') ){
-            $("#otroSalario,#tituloOtroSalario").css("display","block");
-            $("select#salario").prop('disabled', true);
-            $('select').material_select();   
+    $("#checkbox2").click( function(){ //Funcion clic 
+        if( $(this).is(':checked') ){ //Validador el checkbox
+            $("#otroSalario,#tituloOtroSalario").css("display","block"); //Aparece el input de otro salario
+            $("select#salario").prop('disabled', true); //Deshabilita el select
+            $('select').material_select();   //Para materialize
         }else{
-            $("#otroSalario,#tituloOtroSalario").css("display","none");
-            $("select#salario").prop('disabled', false);
-            $('select').material_select();
+            $("#otroSalario,#tituloOtroSalario").css("display","none");//desaparece el input de otro salario
+            $("select#salario").prop('disabled', false);//Habilita el select
+            $('select').material_select();//Para materialize
         }
-     });
+    });
      
 });
 
