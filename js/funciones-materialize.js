022df9d56c1afa91593df4b8ffcc8a673360fc3f
jmontanabonilla <jmontanabonilla@gmail.com>
2018-04-29 18:03:42 -0500

    cambios

--- js/funciones-materialize.js
+++ js/funciones-materialize.js
@@ -10,9 +10,44 @@ $(document).ready(function(){ //Funcion al momento de recargar la página
             $('select').material_select();//Para materialize
         }
     });
+    //variables
+	 var pass1 = $('[name=password]');
+	var pass2 = $('[name=confirm-password]');
+	var confirmacion = "Las contraseñas si coinciden";
+	var longitud = "La contraseña debe estar formada entre 6-16 carácteres (ambos inclusive)";
+	var negacion = "No coinciden las contraseñas";
+	var vacio = "La contraseña no puede estar vacía";
+	//oculto por defecto el elemento span
+	var span = $('<span></span>').insertAfter(pass2);
+	span.hide();
+	//función que comprueba las dos contraseñas
+	function coincidePassword(){
+	var valor1 = pass1.val();
+	var valor2 = pass2.val();
+	//muestro el span
+	span.show().removeClass();
+	//condiciones dentro de la función
+	if(valor1 != valor2){
+	span.text(negacion).addClass('negacion');	
+	}
+	if(valor1.length==0 || valor1==""){
+	span.text(vacio).addClass('negacion');	
+	}
+	if(valor1.length<6 || valor1.length>16){
+	span.text(longitud).addClass('negacion');
+	}
+	if(valor1.length!=0 && valor1==valor2){
+	span.text(confirmacion).removeClass("negacion").addClass('confirmacion');
+	}
+	}
+        //ejecuto la función al soltar la tecla
+        pass2.keyup(function(){
+        coincidePassword();
+        });
+    
 
-   
-     
 });
+
+
 
 
