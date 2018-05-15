23ad3fb4bdaaf6a995c8de396a89f0474912fb4c
David Stiven Quiasua Pinilla <dquiasua@heinsohn.com.co>
2018-05-15 17:49:29 -0500

    Si se daña algo no e smi culpa

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-        $('select').material_select();
-
-        // for HTML5 "required" attribute
-        $("select[required]").css({
-          display: "inline",
-          height: 0,
-          padding: 0,
-          width: 0
-        });
-    });    
+   
 
     
