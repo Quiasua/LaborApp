59426c08202635fb026119d922dae52d3220f8a2
yeison135 <yeison6340@gmail.com>
2018-05-19 11:33:29 -0500

    cambios (yalvarez)

--- js/creacion_cuenta.js
+++ js/creacion_cuenta.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
 
     });
 
-    var server = "52.13.153.72";
+    var server = "52.67.195.28";
     $("#siguiente3").click(function () {
         var validarCorreo = $("#correo").val();       
         var espacioCorreo =  validarCorreo.split("@");
