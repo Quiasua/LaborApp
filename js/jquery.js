95c30cd1304e79c3e8ad4f809746f603e60a4907
jmontanabonilla <jmontanabonilla@gmail.com>
2018-05-20 16:47:10 -0500

    prescripcion

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,49 @@
-   
+$(document).ready(function () {
+    $(".datepicker").pickadate({
+      closeOnSelect: true,
+      format: "dd/mm/yyyy",
+      close: 'Seleccionar',
+      today: false,
+      clear: false,
+    });
+
+    $('#siguiente3').click(function(){
+    //Prescripcion    
+        var f = new Date();
+        var dia = f.getDate();
+        var mes =(f.getMonth() +1) ;
+        var anio = f.getFullYear();
+        var fechafin = $("#fechaFinal").val();
+        var arregloFecha  = fechafin.split('/');
+        var diafin = parseInt(arregloFecha[0]);
+        var mesfin = parseInt(arregloFecha[1]);
+        var aniofin = parseInt(arregloFecha[2])+3;
+    
+        if(aniofin >= anio){
+           if(mesfin >= mes){
+                }if(diafin>= dia){
+                    window.location.href = "correo.html";
+                }else{
+                    window.location.href = "abogado.html";
+                }
+        } 
+     
+     
+        var salario = $("#otroSalario").val();
+      console.log(salario,"salario")
+      var cuantia = 781242 * 20;
+      console.log(cuantia,"cuenta")
+      if(salario >= cuantia){
+        window.location.href = "abogado.html";
+      }
+      
+     
+
+      
+      
+    })
+  
+  
+  });   
 
     
