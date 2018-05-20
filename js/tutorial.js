5f985000a68e14cd67ff9b5c5b96deceb90e9ef8
David Stiven Quiasua Pinilla <dquiasua@heinsohn.com.co>
2018-05-20 17:44:42 -0500

    cambiecitos

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,32 +1,29 @@
-$(document).ready(function () {  
-
-
-
+$(document).ready(function () {
     $("#btn-enviar").click(function () {
-            var filtro = {
-                filtroId: $("#user").val()
+        var filtro = {
+            filtroId: $("#user").val()
+        }
+        var link = document.URL;
+        var urls = new URL(link);
+        var id = urls.searchParams.get("user");
+        window.location.href = "main.html?user=" + id;
+        /*
+        var server = "localhost";
+        var url_persona = "http://" + server + ":8080/laborapp/api/legalapp/actualizarTutorial";
+        $.ajax({
+            url: url_persona,
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(filtro),
+            contentType: 'application/json',
+            beforeSend: function (request) {
+                request.setRequestHeader("Authorization", "Admin");
+            },
+            success: function (data) {
+                console.log(data);
             }
-            var link = document.URL;
-            var urls = new URL(link);
-            var id = urls.searchParams.get("user");
-            window.location.href = "main.html?user=" + id;
-            /*
-            var server = "localhost";
-            var url_persona = "http://" + server + ":8080/laborapp/api/legalapp/actualizarTutorial";
-            $.ajax({
-                url: url_persona,
-                type: 'POST',
-                dataType: 'json',
-                data: JSON.stringify(filtro),
-                contentType: 'application/json',
-                beforeSend: function (request) {
-                    request.setRequestHeader("Authorization", "Admin");
-                },
-                success: function (data) {
-                    console.log(data);
-                }
-            })
-            */
+        })
+        */
     })
 
 
@@ -36,4 +33,4 @@ $(document).ready(function () {
 
     $('#modal1').show();
     $('.modal').modal();
-});    
+});
