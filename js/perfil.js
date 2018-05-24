0fd2e9dfba98ad6fe53010774f7e38625b0f3643
yeison135 <yeison6340@gmail.com>
2018-05-24 12:48:43 -0500

    cambos

---
+++ js/perfil.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    var link = document.URL;
+    var urls = new URL(link);
+    var id = urls.searchParams.get("user");
+    var idSer = Number(id);
+    var filtro = {
+        filtroId: idSer
+    }
+    var server = "localhost";
+    var url_persona = "http://" + server + ":8080/laborapp/api/legalapp/consultarUsuarioId";
+    $.ajax({
+        url: url_persona,
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(filtro),
+        contentType: 'application/json',
+        beforeSend: function (request) {
+            request.setRequestHeader("Authorization", "Admin");
+        },
+        success: function (data) {
+            window.location.href = "main.html?user=" + id;
+            console.log(data);
+        }
+    })
+
+
+
+    $("#btn-enviar").click(function () {
+        var link = document.URL;
+        var urls = new URL(link);
+        var id = urls.searchParams.get("user");
+        var idSer = Number(id);
+        var filtro = {
+            idUsuario: idSer
+        }
+        var server = "localhost";
+        var url_persona = "http://" + server + ":8080/laborapp/api/legalapp/actualizarTutorial";
+        $.ajax({
+            url: url_persona,szXAZ
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(filtro),
+            contentType: 'application/json',
+            beforeSend: function (request) {
+                request.setRequestHeader("Authorization", "Admin");
+            },
+            success: function (data) {
+                window.location.href = "main.html?user=" + id;
+                console.log(data);
+            }
+        })
+    })
+    $('#modal1').show();
+    $('.modal').modal();
+});
